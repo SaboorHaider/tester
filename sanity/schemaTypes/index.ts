412108668c0200f423f516b3855a0a3be6dd4bd0
Saboor_Haider <162649541+SaboorHaider@users.noreply.github.com>
2025-02-06 07:53:41 +0500

    Delete sanity directory

--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import chefs from './chefs'
-import foods from './foods'
-import blog from './blog'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [chefs, foods , blog],
-}
